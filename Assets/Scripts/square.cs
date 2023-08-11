c5556ec6dece434d829489e1fd6309b43f50f4a9
KHS <kij4300@naver.com>
2023-08-11 13:07:44 +0900

    네모 랜덤 위치

--- Assets/Scripts/square.cs
+++ Assets/Scripts/square.cs
@@ -7,7 +7,10 @@ public class square : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        float x = Random.Range(-3f, 3f);
+        float y = Random.Range(3f, 5f);
+
+        transform.position = new Vector3(x, y, 0);
     }
 
     // Update is called once per frame
