3d6d68fbe105b71e545f8389455eb4f859e260f2
KHS <kij4300@naver.com>
2023-08-11 13:36:46 +0900

    시간 증가

--- Assets/Scripts/gameManager.cs
+++ Assets/Scripts/gameManager.cs
@@ -1,13 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class gameManager : MonoBehaviour
 {
     public GameObject square;
+    public Text timeTxt;
 
-    // Start is called before the first frame update
-    void Start()
+    float alive = 0f;
+
+// Start is called before the first frame update
+void Start()
     {
         InvokeRepeating("makeSquare", 0f, 0.5f);
     }
@@ -15,7 +19,8 @@ public class gameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        alive += Time.deltaTime;
+        timeTxt.text = alive.ToString("N2");
     }
 
     void makeSquare()
